73a6f00c69d83efa92132d943cd5dc0b19a91c2b
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-07-19 13:38:56 +0300

    feat: create conn between all tg bot comp

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,77 @@
 package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/DimTur/tg_news_bot_go/internal/config"
+	"github.com/DimTur/tg_news_bot_go/internal/fetcher"
+	"github.com/DimTur/tg_news_bot_go/internal/notifier"
+	"github.com/DimTur/tg_news_bot_go/internal/storage"
+	"github.com/DimTur/tg_news_bot_go/internal/summary"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jmoiron/sqlx"
+)
+
+func main() {
+	botAPI, err := tgbotapi.NewBotAPI(config.Get().TelegramBotToken)
+	if err != nil {
+		log.Printf("failed to create bot: %v", err)
+		return
+	}
+
+	db, err := sqlx.Connect("postgres", config.Get().DatabaseDSN)
+	if err != nil {
+		log.Printf("failed to connect to database: %v", err)
+		return
+	}
+	defer db.Close()
+
+	var (
+		articleStorage = storage.NewArticleStorage(db)
+		sourceStorage  = storage.NewSourceStorage(db)
+		fetcher        = fetcher.New(
+			articleStorage,
+			sourceStorage,
+			config.Get().FetchInterval,
+			config.Get().FilterKeywords,
+		)
+		notifier = notifier.New(
+			articleStorage,
+			summary.NewOpenAISummarizer(config.Get().OpenAIKey, config.Get().OpenAIPromt),
+			botAPI,
+			config.Get().NotificationInterval,
+			2*config.Get().FetchInterval,
+			config.Get().TelegramChannelID,
+		)
+	)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	go func(ctx context.Context) {
+		if err := fetcher.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to start fetcher: %v", err)
+				return
+			}
+
+			log.Printf("fetcher stopped")
+		}
+	}(ctx)
+
+	// go func(ctx context.Context) {
+	if err := notifier.Start(ctx); err != nil {
+		if !errors.Is(err, context.Canceled) {
+			log.Printf("[ERROR] failed to start notifier: %v", err)
+			return
+		}
+
+		log.Printf("notifier stopped")
+	}
+	// }(ctx)
+}
